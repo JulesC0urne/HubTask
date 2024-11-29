85a6bf707efc9fc4ffc87de07dbf79a728143830
JulesC0urne <jules.courne@gmail.com>
2024-11-29 20:32:54 +0100

    commit 1

--- app-task/task-app/src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './App'; // adjust the import as necessary
-
-test('renders AuthScreen at the root path', () => {
-  // Render the App component directly, assuming it contains a Router.
-  render(<App />); // No need to wrap it in MemoryRouter
-});
